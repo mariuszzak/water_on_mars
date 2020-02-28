08005e77b397ca39f212cd170092299280b4183f
Mariusz <mariuszzak21@gmail.com>
2020-02-28 12:02:29 +0100

    Generate Mix app

---
+++ lib/water_on_mars.ex
@@ -0,0 +1,18 @@
+defmodule WaterOnMars do
+  @moduledoc """
+  Documentation for `WaterOnMars`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WaterOnMars.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/water_on_mars_test.exs
@@ -0,0 +1,8 @@
+defmodule WaterOnMarsTest do
+  use ExUnit.Case
+  doctest WaterOnMars
+
+  test "greets the world" do
+    assert WaterOnMars.hello() == :world
+  end
+end
