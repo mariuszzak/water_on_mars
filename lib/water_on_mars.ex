9123a8c36f9758fba902ac743e24b43d99b4afd8
Mariusz <mariuszzak21@gmail.com>
2020-02-28 17:12:17 +0100

    Implement the scores_grid/1 in the main module

--- lib/water_on_mars.ex
+++ lib/water_on_mars.ex
@@ -53,4 +53,44 @@ defmodule WaterOnMars do
     end)
     |> Enum.join("\n")
   end
+
+  @doc """
+  The function processes sensor data and returns the rated locations in a form of two-dimensional list.
+  It is intended for visualizing the results in a form of a heatmap.
+
+  ## Examples
+
+      iex> WaterOnMars.scores_grid("1 3 1 2 3 4 5 6 7 8 9")
+      {:ok, [[12, 21, 16], [27, 45, 33], [24, 39, 28]]}
+
+      iex> WaterOnMars.scores_grid("x y z")
+      {:error, "Input is invalid."}
+
+      iex> WaterOnMars.scores_grid("0 1 2")
+      {:error, "requested_results_number must be greater than 0"}
+
+  """
+
+  @type score :: integer()
+  @type scores_grid :: list(list(score()))
+
+  @spec scores_grid(binary()) :: {:ok, scores_grid()} | {:error, String.t()}
+  def scores_grid(input_binary) do
+    with {:ok, parsed_input} <- WaterOnMars.InputParser.call(input_binary),
+         :ok <- WaterOnMars.InputValidator.call(parsed_input) do
+      {:ok, do_generate_scores_grid(parsed_input)}
+    else
+      {:error, :invalid_input} -> {:error, "Input is invalid."}
+      {:error, error} -> {:error, error}
+    end
+  end
+
+  defp do_generate_scores_grid(%{
+         grid_size: grid_size,
+         raw_sensor_data: raw_sensor_data
+       }) do
+    raw_sensor_data
+    |> WaterOnMars.WaterConcentrationMap.new(grid_size)
+    |> WaterOnMars.LocationRater.scores_grid()
+  end
 end
